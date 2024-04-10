f852e78f559a51b5213f30e5e19b96d2e600776d
Arseniy Nikulin <studenq.work@gmail.com>
2024-04-10 20:49:22 +0400

    commit fix

--- templates/roadmaps/script.js
+++ templates/roadmaps/script.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// function toggleContent(id) {
-//     var content = document.getElementById(id);
-//     var allContents = document.querySelectorAll('.content');
-
-//     // Сначала закрываем все открытые блоки
-//     allContents.forEach(function(otherContent) {
-//         if (otherContent.id !== id) {
-//             otherContent.classList.remove('expanded');
-//         }
-//     });
-
-//     // Затем переключаем состояние выбранного блока
-//     content.classList.toggle('expanded');
-// }
-
-function toggleContent(contentId) {
-    // Находим элемент с id, соответствующим переданному contentId
-    var content = document.getElementById(contentId);
-    
-    // Переключаем класс 'expanded' для контента
-    content.classList.toggle('expanded');
-    
-    // Находим родительский элемент 'stage', чтобы получить к нему доступ
-    var stage = content.closest('.stage');
-    
-    // В родительском элементе находим <img> и переключаем класс 'flipped'
-=======
 function toggleContent(contentId) {
     var content = document.getElementById(contentId);
     
@@ -33,7 +5,6 @@ function toggleContent(contentId) {
     
     var stage = content.closest('.stage');
     
->>>>>>> 66677e9 (first commit)
     var arrow = stage.querySelector('img');
     if (arrow) {
         arrow.classList.toggle('flipped');
